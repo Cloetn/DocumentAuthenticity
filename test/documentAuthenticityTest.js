369732fbd8051e6c4b1ccc9e19f7b480ff636745
Kevin Cloet (Surface) <cloetkevin @>
2018-04-14 15:11:39 +0200

    Added extra testing and spllited the CMC creation to a seperate method

--- test/documentAuthenticityTest.js
+++ test/documentAuthenticityTest.js
@@ -15,8 +15,7 @@ contract('CMC', function(accounts) {
     var crdbInstance;
     var hash="123456778993";
     
-
-    it("Should up upload document and check if it exists",function(){
+    it ("Should set up the whole CMC system",function(){
         return CMC.deployed().then(function(instance){
             cmcInstance = instance;
         }).then(function(){
@@ -31,13 +30,34 @@ contract('CMC', function(accounts) {
             return cmcInstance.addContract(web3.fromAscii("CRManager"),CRManager.address);
         }).then(function(){
             return cmcInstance.addContract(web3.fromAscii("CRDB"),crdbInstance.address);
-        }).then(function(){
-            return crManagerInstance.uploadDocument(web3.fromAscii(hash),web3.fromAscii("My Document"),
-            web3.fromAscii("Kevin"),web3.fromAscii("test@test.be"));
-        }).then(function(){            
-            return crManagerInstance.documentExists(web3.fromAscii(hash));
-        }).then(function(result){
-            assert.isTrue(result);
         });
     });
+
+    it("Should up upload document and check if it exists",function(){
+        return crManagerInstance.uploadDocument(web3.fromAscii(hash),web3.fromAscii("My Document"),
+            web3.fromAscii("Kevin"),web3.fromAscii("test@test.be")).
+        then(function(){            
+            return crManagerInstance.documentExists(web3.fromAscii(hash));
+        }).then(function(result){
+            assert.isTrue(result,"Document was not uploaded.");
+        });
+    });
+
+    it("Should get the details of a document",function(){
+        return crManagerInstance.getDocument(web3.fromAscii(hash)).
+        then(function(result){
+            var exists = result[0];
+            assert.isTrue(exists,"Document does not exist");
+
+            var title = web3.toAscii(result[1]).replace(/\u0000/g,'');
+            assert.equal(title,"My Document","Title is not the same.");
+
+            var author = web3.toAscii(result[2]).replace(/\u0000/g,'');
+            assert.equal(author,"Kevin","Author is not the same.");
+
+            var email = web3.toAscii(result[3]).replace(/\u0000/g,'');
+            assert.equal(email,"test@test.be","Email is not the same");
+        });
+    });
+
 });
